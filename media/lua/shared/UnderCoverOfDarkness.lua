a113fd8196731c2c2d87fa3179184be74c941f5d
HammyPig <jamesbwalden@protonmail.com>
2023-03-31 12:40:30 +1100

    add is night time function

--- media/lua/shared/UnderCoverOfDarkness.lua
+++ media/lua/shared/UnderCoverOfDarkness.lua
@@ -2,10 +2,13 @@ local SIGHT_EAGLE = 1
 local SIGHT_NORMAL = 2
 local SIGHT_POOR = 3
 
-local function underCoverOfDarkness()
+local function isNightTime()
     local hour = getGameTime():getTimeOfDay()
+    return hour >= 23 or hour < 5
+end
 
-    if hour < 5 or hour > 23 then
+local function underCoverOfDarkness()
+    if isNightTime() then
         getSandboxOptions():set("ZombieLore.Sight", SIGHT_POOR)
     else
         getSandboxOptions():set("ZombieLore.Sight", SIGHT_NORMAL)
