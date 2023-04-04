8f2b419aaa94304fdcf42d1007d0d287fcc0d0e1
HammyPig <jamesbwalden@protonmail.com>
2023-04-04 13:06:06 +1000

    remove debug mode

--- media/lua/shared/UnderCoverOfDarkness.lua
+++ media/lua/shared/UnderCoverOfDarkness.lua
@@ -1,5 +1,3 @@
-local DEBUG_MODE = true
-
 local SIGHT_EAGLE = 1
 local SIGHT_NORMAL = 2
 local SIGHT_POOR = 3
@@ -26,43 +24,11 @@ local function isFoggy(climateManager)
     return fogIntensity >= SandboxVars.UnderCoverOfDarkness.MinimumFogIntensity
 end
 
-local function printValues(climateManager)
-    local function getZombieSightName()
-        local zombieSightNames = {"Eagle", "Normal", "Poor"}
-        local zombieSight = getSandboxOptions():getOptionByName("ZombieLore.Sight"):getValue()
-
-        return zombieSightNames[zombieSight]
-    end
-
-    local hour = getGameTime():getTimeOfDay()
-    local season = climateManager:getCurrentDay():getSeason()
-
-    local nightStartHour = season:getDusk() + DUSK_OFFSET
-    local nightEndHour = season:getDawn() + DAWN_OFFSET
-
-    local fogIntensity = climateManager:getFogIntensity()
-
-    print("hour: ", hour)
-    print("nightStartHour: ", nightStartHour)
-    print("nightEndHour: ", nightEndHour)
-    print()
-    print("fogIntensity: ", fogIntensity)
-    print("minimumFogIntensity: ", SandboxVars.UnderCoverOfDarkness.MinimumFogIntensity)
-    print()
-    print("isNightTime: ", isNightTime(climateManager))
-    print("isFoggy: ", isFoggy(climateManager))
-    print("zombieSightName: ", getZombieSightName())
-end
-
 local function underCoverOfDarkness(climateManager)
     if isNightTime(climateManager) or isFoggy(climateManager) then
         getSandboxOptions():set("ZombieLore.Sight", SIGHT_POOR)
     else
         getSandboxOptions():set("ZombieLore.Sight", SIGHT_NORMAL)
-    end
-
-    if DEBUG_MODE then
-        printValues(climateManager)
     end
 end
 
